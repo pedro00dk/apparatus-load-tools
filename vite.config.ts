52c93cb956271a1161cc73993c4f1b44b4453fdb
Pedro Henrique <pedro00dk@gmail.com>
2025-07-25 16:32:03 -0300

    Add solid js as optional dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
+import solid from 'vite-plugin-solid'
 
 export default defineConfig({
     build: {
-        lib: { entry: ['./src/index.ts'], formats: ['es'] },
+        lib: { entry: ['./src/index.ts', './src/solid.tsx'], formats: ['es'] },
         rollupOptions: { external: ['solid-js'] },
     },
+    plugins: [solid()],
 })
