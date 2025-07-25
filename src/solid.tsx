b8c2898eb105ed242269df2170a89cc9243a9ac3
Pedro Henrique <pedro00dk@gmail.com>
2025-07-25 16:50:06 -0300

    Implement a skeleton generator for solid-js Show

--- src/solid.tsx
+++ src/solid.tsx
@@ -1,4 +1,14 @@
-import { children, createComputed, createContext, createMemo, createSignal, JSX, Suspense, useContext } from 'solid-js'
+import {
+    children,
+    createComputed,
+    createContext,
+    createMemo,
+    createSignal,
+    JSX,
+    Show,
+    Suspense,
+    useContext,
+} from 'solid-js'
 import { KebabCase } from 'type-fest'
 import { inject, type SkeletonOptions } from './skeleton.ts'
 
@@ -35,13 +45,16 @@ export const sk = () => useContext(SkeletonContext)
  * A SolidJS {@linkcode Suspense} wrapper that constructs a skeleton page automatically based on `children`.
  *
  * It works by rendering the children twice, once for the {@linkcode Suspense} `children`, and another for `fallback`.
- * The `fallback` is resolved ahead of time to avoid triggering `Suspense` layers above the current one, and to allow
- * HTML introspection.
+ * The `fallback` is resolved ahead of time to avoid triggering `Suspense` layers above, and to allow introspection.
  *
  * Note that for the skeleton page work properly, the content of the loading page must te stable. Otherwise, the
  * generated skeleton will be changing together with the loading page content. In order to help with page stabilization,
  * {@linkcode SkeletonContext} and {@linkcode sk} utilities are injected only in the `fallback` page to help adding
  * stabilization conditions.
+ *
+ * @param target Target to inject the skeleton container.
+ * @param debug Enable debug mode.
+ * @param children Children to render and generate skeletons for.
  */
 export const SuspenseSkeleton = (props: {
     //
@@ -60,5 +73,35 @@ export const SuspenseSkeleton = (props: {
             {(() => setSuspended(false)) as unknown as undefined}
             {props.children}
         </Suspense>
+    )
+}
+
+/**
+ * A SolidJS {@linkcode Show} wrapper that constructs a skeleton page automatically based on `children`.
+ *
+ * It works by rendering the children twice, once for the {@linkcode Show} `children`, and another for `fallback`.
+ * The `fallback` is resolved ahead of time to avoid triggering `Suspense` layers above, and to allow introspection.
+ *
+ * @param when {@linkcode Show} when property.
+ * @param target Target to inject the skeleton container.
+ * @param debug Enable debug mode.
+ * @param children Children to render and generate skeletons for.
+ */
+export const ShowSkeleton = (props: {
+    //
+    when: boolean
+    target: Element | ShadowRoot
+    debug?: boolean
+    children?: JSX.Element
+}) => {
+    const resolved = children(() => <SkeletonContext.Provider value={true} children={props.children} />)
+    const elements = createMemo(() => resolved.toArray().filter(element => element instanceof HTMLElement))
+    const cleanup = createMemo(() => inject(props.target, elements(), !!props.debug)())
+    createComputed(() => props.when && cleanup())
+
+    return (
+        <Show when={props.when} fallback={elements()}>
+            {props.children}
+        </Show>
     )
 }
