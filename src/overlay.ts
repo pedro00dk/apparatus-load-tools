02edac06bed353e01546e9cdb36865488b21dc4d
Pedro Henrique <pedro00dk@gmail.com>
2025-07-28 00:41:45 -0300

    Fix names in overlay

--- src/overlay.ts
+++ src/overlay.ts
@@ -29,20 +29,20 @@ const configuration = {
     defaults: { ovIn: '200', ovOut: '200', ovZ: '1' } satisfies OverlayOptions,
     /** Render function for the overlay element. */
     create: () => {
-        const decoration = document.createElement('i')
-        decoration.style.position = 'absolute'
-        decoration.style.display = 'grid'
-        decoration.style.placeItems = 'center'
-        decoration.style.background = '#FFFFFFC0'
-        decoration.style.inset = '0'
+        const overlay = document.createElement('i')
+        overlay.style.position = 'absolute'
+        overlay.style.display = 'grid'
+        overlay.style.placeItems = 'center'
+        overlay.style.background = '#FFFFFFC0'
+        overlay.style.inset = '0'
         const spinnerTemplate = document.createElement('template')
         spinnerTemplate.innerHTML = `<svg fill="none" width="50" height="50" stroke="#1A2126" stroke-width="6">
             <circle cx="25" cy="25" r="22" opacity=".3"/>
             <path d="M25 3a22 22 0 0 1 22 22" stroke-linecap="round" />
         </svg>`
-        const spinner = decoration.appendChild(spinnerTemplate.content.firstChild as SVGElement)
+        const spinner = overlay.appendChild(spinnerTemplate.content.firstChild as SVGElement)
         spinner.animate({ rotate: ['0turn', '1turn'] }, { duration: 1000, iterations: Infinity })
-        return decoration
+        return overlay
     },
 }
 
@@ -63,11 +63,11 @@ export const setOverlayConfiguration = (overrides: Partial<typeof configuration>
  *
  * Elements side effects:
  * - `element.style.position`: Set to `relative`.
- * - `element.children`: Decoration appended.
+ * - `element.children`: Overlay appended.
  *
- * Decorations side effects:
- * - `decoration.slot`: If required using `dataset` options.
- * - `decoration.style`: Several positioning properties.
+ * Overlay side effects:
+ * - `overlay.slot`: If required using `dataset` options.
+ * - `overlay.style`: Several positioning properties.
  *
  * A cleanup function is returned to unsubscribe listeners and remove the overlay.
  *
@@ -76,30 +76,30 @@ export const setOverlayConfiguration = (overrides: Partial<typeof configuration>
 export const injectOverlay = (element: HTMLElement) => {
     const inject = (animate: boolean) => {
         const options = { ...configuration.defaults, ...element.dataset }
-        const decoration = configuration.create()
-        decoration.slot = options.ovSlot ?? ''
-        decoration.style.position = 'absolute'
-        decoration.style.inset = '0'
-        decoration.style.zIndex = options.ovZ
-        decoration.slot = options.ovSlot ?? ''
+        const overlay = configuration.create()
+        overlay.slot = options.ovSlot ?? ''
+        overlay.style.position = 'absolute'
+        overlay.style.inset = '0'
+        overlay.style.zIndex = options.ovZ
+        overlay.slot = options.ovSlot ?? ''
         element.style.position = 'relative'
-        element._overlay = decoration
-        element.append(decoration)
+        element._overlay = overlay
+        element.append(overlay)
         resizeObserver.observe(element)
         const duration = +options.ovIn * +animate
-        requestAnimationFrame(() => decoration.animate({ opacity: [0, 1] }, { duration, easing: 'ease-out' }))
+        requestAnimationFrame(() => overlay.animate({ opacity: [0, 1] }, { duration, easing: 'ease-out' }))
     }
 
     const eject = (animate: boolean) => {
         const options = { ...configuration.defaults, ...element.dataset }
-        const decoration = element._overlay
+        const overlay = element._overlay
         element._overlay = undefined
         resizeObserver.unobserve(element)
         const duration = +options.ovOut * +animate
         requestAnimationFrame(() =>
-            decoration
+            overlay
                 ?.animate({ opacity: [1, 0] }, { duration, easing: 'ease-in' })
-                .finished.then(() => decoration?.remove()),
+                .finished.then(() => overlay?.remove()),
         )
     }
 
